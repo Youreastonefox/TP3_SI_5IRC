73daa0951c5e7b5d01ae58465ada162fb68043c3
Youreastonefox <louisa.chd@gmail.com>
2023-10-06 16:52:06 +0200

    Classes du modèle de données

---
+++ TP3/Models/EntityFramework/Notation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP3.Models.EntityFramework;
+
+[Table("t_j_notation_not")]
+public partial class Notation
+{
+    [Column("utl_id")]
+    public int UtilisateurId { get; set; }
+
+    [Column("ser_id")]
+    public int SerieId { get; set; }
+
+    [Column("not_note")]
+    public int Note { get; set; }
+
+    [ForeignKey(nameof(Utilisateur))]
+    [InverseProperty("NotesUtilisateur")]
+    public virtual Utilisateur UtilisateurNotant { get; set; } = null!;
+
+    [ForeignKey(nameof(Serie))]
+    [InverseProperty("NotesSerie")]
+    public virtual Serie SerieNotee { get; set; } = null!;
+}
---
+++ TP3/Models/EntityFramework/Utilisateur.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP3.Models.EntityFramework;
+
+[Table("t_e_utilisateur_utl")]
+public partial class Utilisateur
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("utl_id")]
+    public int UtilisateurId { get; set; }
+
+    [Column("utl_nom")]
+    [StringLength(50)]
+    public string? Nom { get; set; }
+
+    [Column("utl_prenom")]
+    [StringLength(50)]
+    public string? Prenom { get; set; }
+
+    [Column("utl_mobile", TypeName="char(10)")]
+    [StringLength(10)]
+    public string? Mobile { get; set; }
+
+    [Column("utl_mail")]
+    [StringLength(100)]
+    [Required]
+    public string? Mail { get; set; }
+
+    [Column("utl_pwd")]
+    [StringLength(64)]
+    [Required]
+    public string? Pwd { get; set; }
+
+    [Column("utl_rue")]
+    [StringLength(200)]
+    public string? Rue { get; set; }
+
+    [Column("utl_cp", TypeName="char(50)")]
+    [StringLength(50)]
+    public string? CodePostal { get; set; }
+
+    [Column("utl_ville")]
+    [StringLength(50)]
+    public string? Ville { get; set; }
+
+    [Column("utl_pays")]
+    [StringLength(50)]
+    public string? Pays { get; set; }
+
+    [Column("utl_latitude")]
+    public float? Latitude { get; set; }
+
+    [Column("utl_longitude")]
+    public float? Longitude { get; set; }
+
+    [Column("utl_datecreation")]
+    [Required]
+    public DateTime DateCreation { get; set; }
+
+    [InverseProperty("UtilisateurNotant")]
+    public virtual ICollection<Notation> NotesUtilisateur { get; set; } = new List<Notation>();
+
+}
